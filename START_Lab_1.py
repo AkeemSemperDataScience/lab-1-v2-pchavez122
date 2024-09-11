ae1a986aa5d538f9e4d479def1a3044f003b761c
Pat <52054814+pchavez3@users.noreply.github.com>
2024-09-10 18:48:46 -0600

    save changes

--- START_Lab_1.py
+++ START_Lab_1.py
@@ -4,14 +4,15 @@ def lab1Question1(input_gb):
     # Do the work here
     # The solution to this goes here (and in all of them below...)
     # Set the variable num_bytes to the answer and return it
-    num_bytes = input_gb * 1024 * 1024 * 1024
+    num_bytes = input_gb * 1024 * 1024 * 1024 # 1024 Bytes > 1 Kilobyte, 1024 Kilobytes > 1 Megabyte, 1024 Megabytes > 1 Gigbyte
     return num_bytes
 
 def lab1Question2(name):
     # Take an input of a name, return True if there is an odd number of characters in the name, False otherwise
     # Return None if the input is not a string
     is_odd = None
-    if type(name) is str:
+    # Check if name is a string type
+    if type(name) is str: 
         if (len(name)%2 > 0):
             is_odd = True
         elif (len(name)%2 == 0):
@@ -22,22 +23,48 @@ def lab1Question3(input_string, input_number):
     # Take in two inputs - a string and a number
     # Return the character of the string in the index given by number.  If this index does not exist, then return -1.
     character_at = -1
-
+    # Checks if the input_number is in range of the lowest index to the highest index number for input_string
+    if (len(input_string) - 1 >= input_number) and (-(len(input_string) - 1) <= input_number): 
+        character_at = input_string[input_number]
     return character_at
 
 def lab1Question4(file_name):
     # Take an input of a file name. 
     # Read that file and return a list of all numbers in that file
     list_of_nums = []
-
+    file = open(file_name)
+    for line in file:
+        print(line)
+        list_of_nums.append(line)
+    file.close()
     return list_of_nums
+
+print(lab1Question4('github/test_file1.txt'))
 
 def lab1Question5(list_numbers):
     # Take an input of a list of numbers
     # Return the mode from that list. 
     mode_of_list = None
-
+    number_dict = {}
+    for number in list_numbers:
+        if not number in number_dict:
+            number_dict[number] = 1
+        else:
+            counter = number_dict[number] + 1
+            number_dict.update({number: counter})
+    print(number_dict)
+    var_mode = 0
+    for key, number in number_dict.items():
+        if number > var_mode:
+            var_mode = number
+            mode_of_list = key
     return mode_of_list
+
+'''
+print("      ")
+my_list = [2,2,1,1]
+print(lab1Question5(my_list))
+'''
 
 def lab1Question6(quarters, dimes, nickels, pennies):
     # Take in 4 inputs - the number of quarters, dimes, nickels, and pennies in a handful
